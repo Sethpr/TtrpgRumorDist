b1fbbf90ea8f0891507660388503de508dad4ce9
seth <sprentice97@gmail.com>
2023-06-07 10:47:31 -0400

    should evenly generate rumors

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         String input = "";
         ArrayList<Player> players = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         System.out.println("how many rumors per player:");
         int rumorNum = in.nextInt();
+        in.nextLine();
+        String rumor;
         while(true){
             System.out.println("Please enter a player or type 'done' if they are all in:");
-            input = in.next().toLowerCase().trim();
+            input = in.nextLine().toLowerCase().trim();
             if(input.equals("done")){
                 break;
             }
@@ -22,8 +26,20 @@ public class Main {
         }
         for(Player player: players){
             for (int i = 0; i < rumorNum; i++) {
-                System.out.println("Please enter rumor #" + (i+1));
+                System.out.println("Please enter rumor #" + (i+1) + " for " + player.name);
+                player.addRumor(in.nextLine());
             }
+        }
+        for(Player player: players){    //on one hand this is inefficient, on the other hand if I ever run for more than
+            for(Player other: players){ //six players and I'll never do more than five rumors so...
+                if(player.equals(other)){
+                    continue;
+                }
+                for (int i = 0; i < rumorNum-1; i++) {
+                    player.receiveRumor(other.name, other.sendRumor());
+                }
+            }
+            System.out.println(player.getMessage());
         }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,50 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Player {
     String name;
-    ArrayList<String> rumors;
+    HashMap<String, Integer> rumorMap;
+    ArrayList<String> receivedRumors;
+    int minValue=0;
 
     public Player(String name){
         this.name = name;
-        rumors = new ArrayList<>();
+        rumorMap = new HashMap<>();
+        receivedRumors = new ArrayList<>();
+    }
+
+    public void addRumor(String rumor){
+        rumorMap.put(rumor, 0);
+    }
+
+
+    public void receiveRumor(String name, String rumor) {
+        receivedRumors.add(name + ": " + rumor);
+    }
+
+    public String sendRumor() {
+        for(String key : rumorMap.keySet()){
+            if(rumorMap.get(key) == minValue){
+                rumorMap.put(key, rumorMap.get(key) + 1);
+                checkMin();
+                return key;
+            }
+        }
+        return ""+minValue;
+    }
+
+    private void checkMin(){
+        if(!rumorMap.values().contains(minValue)){
+            minValue++;
+        }
+    }
+
+    public String getMessage() {
+        StringBuilder message = new StringBuilder("rumors for " + name + "\n");
+        for(String rumor: receivedRumors){
+            message.append(rumor).append("\n");
+        }
+        return message.toString();
     }
 }
